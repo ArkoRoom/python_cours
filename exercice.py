5d39cc5196adde2abd8b6a82e8f5420f3d933a20
Arkoroom <catel.ludovic@gmail.com>
2024-06-19 15:26:30 +0200

    Add f and :.2f to the results

--- exercice.py
+++ exercice.py
@@ -4,7 +4,7 @@ print("***** Exercice 1 *****")
 consommation_kwh = float(input("Veuillez entrer la consommation (en Kwh) de votre Datacenter : "))
 heures_fonctionnement = float(input("Veuillez entrer le nombre d'heures de fonctionnement de votre datacenter : "))
 calcul_conso = consommation_kwh * heures_fonctionnement
-print("Vous avez consommé ", calcul_conso, "Kwh aujourd'hui") 
+print(f"Vous avez consommé {calcul_conso:.2f}Kwh aujourd'hui") 
 
 # Exercice 2 
 # Calculer le cout de refroidessiment d'un Datacenter
@@ -13,7 +13,7 @@ prix_kwh = float(input("Veuillez entrer le prix du Kwh : "))
 conso_heure = float(input("Veuillez entrer le cout de votre consommation par heure : "))
 heure_fonctio_refroi = float(input("Veuillez entrer le nombre d'heure de fonctionnement du systeme de refroidissement : "))
 calcul_cout_refroidissement = prix_kwh * conso_heure * heure_fonctio_refroi
-print("Votre datacenter coute ", calcul_cout_refroidissement, "€ par jour en refroidissement.")
+print(f"Votre datacenter coute {calcul_cout_refroidissement:.2f}€ par jour en refroidissement.")
 
 # Exercice 3
 # Calcul du temps restant sur batterie du Datacenter
@@ -21,7 +21,7 @@ print("***** Exercice 3 *****")
 energie_batterie = float(input("Veuillez entrer l'énergie stockée dans les batterie en Kwh : "))
 conso_data_moyenne_heure = float(input("Veuillez entrer la consommation moyenne du datacenter par heure en Kwh : "))
 calcul_temps_batterie = energie_batterie / conso_data_moyenne_heure
-print("Le datacenter peut tenir sur batterie ", calcul_temps_batterie, " heures")
+print(f"Le datacenter peut tenir sur batterie {calcul_temps_batterie:.2f} heures")
 
 # Exercice 4
 # Verifier si le serveur est en ligne, sans utiliser de conditions
