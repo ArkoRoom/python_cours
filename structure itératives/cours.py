7663f8a64cadf1c918c9c60de8de46c750ac973d
Arkoroom <catel.ludovic@gmail.com>
2024-06-20 13:49:17 +0200

    Ajout des commentaires

--- structure itératives/cours.py
+++ structure itératives/cours.py
@@ -1,13 +1,21 @@
 # Les boucles for
 
+# Compte jusqu'à 10 sans variable précise
 for _ in range(0, 10) : 
     print("Je me répète !")
 
+# Compte en prenant les elements du tableau
 for element in [0, 1, 2, 3, 4, 5] :
     print(element)
 
+# Compte de 0 à 10
 for incr in range (10) :
     print("incr : ", incr)
 
-for incr1 in range (1, 10) : 
+# Compte de 1 à 11 sans prendre la première itération (0)
+for incr1 in range (1, 11) : 
     print("incr1 : ", incr1)
+
+# Compte toutes les 5 itérations
+for i in range (0, 101, 5) :
+    print("iterate : ", i)
