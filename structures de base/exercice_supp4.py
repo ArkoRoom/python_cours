cf9bdadc13bc1f7d36fdd89863d5589e021a14b4
Arkoroom <catel.ludovic@gmail.com>
2024-06-20 12:20:45 +0200

    restructuration du projet

--- structures de base/exercice_supp4.py
+++ structures de base/exercice_supp4.py
@@ -1,5 +1,5 @@
 # Écrire un programme qui demande à l'utilisateur de saisir le nombre de photocopies effectuées. 
-# Le programme doit calculer et afficher le prix à payer en fonction du nombre de photocopies, selon les règles suivantes :
+# # Le programme doit calculer et afficher le prix à payer en fonction du nombre de photocopies, selon les règles suivantes :
 # - Moins de 10 copies : 0,15 euro par copie
 # - Entre 10 et 19 copies : 0,10 euro par copie
 # - 20 copies et plus : 0,05 euro par copie
