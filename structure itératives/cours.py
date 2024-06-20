66377902cde0991c31cf3e42fc940e7273522b1b
Arkoroom <catel.ludovic@gmail.com>
2024-06-20 13:41:44 +0200

    Ajout du cours sur les structures iteratives

---
+++ structure itératives/cours.py
@@ -0,0 +1,13 @@
+# Les boucles for
+
+for _ in range(0, 10) : 
+    print("Je me répète !")
+
+for element in [0, 1, 2, 3, 4, 5] :
+    print(element)
+
+for incr in range (10) :
+    print("incr : ", incr)
+
+for incr1 in range (1, 10) : 
+    print("incr1 : ", incr1)
