134d1cd2da2bfb481d4e6be944dbf523ec5d1e0d
Arkoroom <catel.ludovic@gmail.com>
2024-06-21 12:08:43 +0200

    Correction exo 8

--- structure itératives/exercice_8.py
+++ structure itératives/exercice_8.py
@@ -2,25 +2,32 @@
 # Lorsqu'un prix de 0 est saisi, cela signifie la fin des saisies. 
 # Le programme doit ensuite afficher le total à payer, demander à l'utilisateur le montant de son paiement, et calculer et afficher la monnaie à rendre, en décomposant cette monnaie en billets de 10 euros, 5 euros, et en pièces de 1 euro.
 
-prix_article = int(input("Prix de l'article : "))
-total_article = int()
+total_prix_article = 0.0
 
-while prix_article > 0 : 
-    prix_article += total_article
-    prix_article = float(input("Prix de l'article : "))
+print("Merci de saisir les prix des articles un par un. Entrez")
+while True : 
+    prix = float(input("Prix de l'article (en euros) : "))
+    if prix == 0 : 
+        break
+    if prix > 0 : 
+        total_prix_article += prix
+    else : 
+        print("Merci de saisir un prix positif.")
 
-print(f"Le total à payer est de : {total_article}€.")
-porte_monnaie = int(input("Quel est votre montant pour régler ? "))
+print(f"Total à payer : {total_prix_article:2f}€")
 
-total_rendu_monnaie = porte_monnaie - total_article
-nb_billet10 = total_rendu_monnaie % 10
-nb_billet5 = 0
-nb_piece = 0
-if nb_billet10 % 10 != 0 : 
-    nb_billet5 = nb_billet10 / 5
-    print(f"Total rendu : {total_rendu_monnaie} soit {nb_billet10} de 10€, {nb_billet5} billets de 5€.")
-elif nb_billet5 != 0 : 
-    nb_piece = nb_billet5 / 1
-    print(f"Total rendu : {total_rendu_monnaie} soit {nb_billet10} de 10€, {nb_billet5} billets de 5€ et {nb_piece} de 1€.")
-else : 
-    print(f"Total rendu : {total_rendu_monnaie} soit {nb_billet10} de 10€.")
+montant_paye = float(input("Merci de saisir le montant que vous payez (en euros) : "))
+
+if montant_paye >= total_prix_article : 
+    monnaie = montant_paye - total_prix_article
+    billets_de_10 = int(monnaie // 10)
+    billets_de_5 = int(monnaie // 5)
+    monnaie = monnaie % 5
+    pieces_de_1 = int(monnaie)
+
+    print(f"Monnaie à rendre : {montant_paye - total_prix_articles:.2f}euros")
+    print(f"Billets de 10 euros : {billets_de_dix}")
+    print(f"Billets de 5 euros : {billets_de_cinq}")
+    print(f"pièce de 1 euro : {pieces_de_un}")
+else:
+    print("Le montant payé doit être plus grand que le total des articles")
