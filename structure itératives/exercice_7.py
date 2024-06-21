9a5a0eb11a40180b5b4161ed32248cd1ef816735
Arkoroom <catel.ludovic@gmail.com>
2024-06-21 10:38:25 +0200

    Fix error in ex 7

--- structure itératives/exercice_7.py
+++ structure itératives/exercice_7.py
@@ -1,8 +1,12 @@
 # Écrire un programme qui demande à l'utilisateur de saisir un nombre de départ. 
 # Le programme doit ensuite calculer et afficher la factorielle de ce nombre.
 
-start_number = int(input("Veuillez saisir un nombre : "))
-error_saisi = False
+while True : 
+    start_number = int(input("Veuillez saisir un nombre : "))
+    if start_number < 0 : 
+        print("Erreur de saisi. Veuillez saisir un chiffre supérieur ou égal à zéro.")
+    else : 
+        break
 
 if start_number == 0 : 
     result = 1
@@ -10,10 +14,4 @@ elif start_number > 0 :
     result = 1 
     for i in range(1, start_number + 1) : 
         result *= i
-else :
-    error_saisi = True
-
-if error_saisi: 
-    print("Erreur de saisi. Veuillez saisir un chiffre supérieur ou égal à zéro.")
-else : 
-    print(f"Le factorielle de {start_number} est : {result}")
+print(f"Le factorielle de {start_number} est : {result}")
