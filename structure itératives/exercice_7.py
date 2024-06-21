41fc292547726f69355d9dad03b8ff9027a67df9
Arkoroom <catel.ludovic@gmail.com>
2024-06-21 10:29:05 +0200

    Correction exercice 7

--- structure itératives/exercice_7.py
+++ structure itératives/exercice_7.py
@@ -2,8 +2,18 @@
 # Le programme doit ensuite calculer et afficher la factorielle de ce nombre.
 
 start_number = int(input("Veuillez saisir un nombre : "))
-result = 1
-for i in range(1, start_number + 1) :
-    result *= i
+error_saisi = False
 
-print(f"Le factorielle de {start_number} est : {result}")
+if start_number == 0 : 
+    result = 1
+elif start_number > 0 : 
+    result = 1 
+    for i in range(1, start_number + 1) : 
+        result *= i
+else :
+    error_saisi = True
+
+if error_saisi: 
+    print("Erreur de saisi. Veuillez saisir un chiffre supérieur ou égal à zéro.")
+else : 
+    print(f"Le factorielle de {start_number} est : {result}")
--- structure itératives/exercice_8.py
+++ structure itératives/exercice_8.py
@@ -1,3 +1,26 @@
-Écrire un programme de caisse qui permet à l'utilisateur de saisir le prix de plusieurs articles un par un. 
-Lorsqu'un prix de 0 est saisi, cela signifie la fin des saisies. 
-Le programme doit ensuite afficher le total à payer, demander à l'utilisateur le montant de son paiement, et calculer et afficher la monnaie à rendre, en décomposant cette monnaie en billets de 10 euros, 5 euros, et en pièces de 1 euro.
+# Écrire un programme de caisse qui permet à l'utilisateur de saisir le prix de plusieurs articles un par un. 
+# Lorsqu'un prix de 0 est saisi, cela signifie la fin des saisies. 
+# Le programme doit ensuite afficher le total à payer, demander à l'utilisateur le montant de son paiement, et calculer et afficher la monnaie à rendre, en décomposant cette monnaie en billets de 10 euros, 5 euros, et en pièces de 1 euro.
+
+prix_article = int(input("Prix de l'article : "))
+total_article = int()
+
+while prix_article > 0 : 
+    prix_article += total_article
+    prix_article = float(input("Prix de l'article : "))
+
+print(f"Le total à payer est de : {total_article}€.")
+porte_monnaie = int(input("Quel est votre montant pour régler ? "))
+
+total_rendu_monnaie = porte_monnaie - total_article
+nb_billet10 = total_rendu_monnaie % 10
+nb_billet5 = 0
+nb_piece = 0
+if nb_billet10 % 10 != 0 : 
+    nb_billet5 = nb_billet10 / 5
+    print(f"Total rendu : {total_rendu_monnaie} soit {nb_billet10} de 10€, {nb_billet5} billets de 5€.")
+elif nb_billet5 != 0 : 
+    nb_piece = nb_billet5 / 1
+    print(f"Total rendu : {total_rendu_monnaie} soit {nb_billet10} de 10€, {nb_billet5} billets de 5€ et {nb_piece} de 1€.")
+else : 
+    print(f"Total rendu : {total_rendu_monnaie} soit {nb_billet10} de 10€.")
