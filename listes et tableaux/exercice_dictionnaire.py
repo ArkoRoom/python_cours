d406e36af4e2b58f3ac8aa5ab3138bc31432fb7a
Arkoroom <catel.ludovic@gmail.com>
2024-06-24 11:00:47 +0200

    Ajout de la correction exercice dictionnaire

--- listes et tableaux/exercice_dictionnaire.py
+++ listes et tableaux/exercice_dictionnaire.py
@@ -9,55 +9,56 @@
 # - Pour ce faire, vous utiliserez des clés de type string qui représenteront les différentes lignes de l'adresse dans le dictionnaire. 
 # - Le logiciel devra permettre l'ajout, l'édition, la suppression et la visualisation des données par l'utilisateur. 
 
-choix_menu = None
-edit_adresse = None
-dico_adresse = {}
-dico_adresses = []
+adresses = [] #  ou un set
 
 while True:
     print('''
-          1 - Ajouter une adresse
-          2 - Modifier une adresse
-          3 - Supprimer une adresse
-          4 - Consulter la liste des adresses
-          0 - Quitter
-    ''')
+          1. Ajouter une adresse
+          2. Editer une adresse
+          3. Supprimer une adresse
+          4. Afficher les adresses
+          0. Quitter''')
     choix_menu = input("Merci d'indiquer votre choix : ")
     match choix_menu:
         case "1":
-            print("***** Ajout d'une adresse *****")
-            add_street_number = int(input("Numéro de voie : "))
-            add_street_complement = input("Complement d'adresse : ")
-            add_street_name = input("Nom de voie : ")
-            add_town = input("Ville : ")
-            add_postcode = int(input("Code Postal : "))
-            dico_adresse["numéro"] = add_street_number
-            dico_adresse["complément"] = add_street_complement
-            dico_adresse["nom de voie"] = add_street_name
-            dico_adresse["ville"] = add_town
-            dico_adresse["code postal"] = add_postcode
-            dico_adresses.append(dico_adresse)
-            print(dico_adresses)
+            print("===== Ajout d'adresse =====")
+            nouvelle_adresse = {}
+            numero_voie = input("Merci de saisir numéro de voie : ")
+            nouvelle_adresse["numero_voie"] = numero_voie
+            complement_adresse = input("Merci de saisir le complément d'adresse : ")
+            nouvelle_adresse["complement_adresse"] = complement_adresse
+            intitule_voie = input("Merci de saisir l'intitulé de la voie : ")
+            nouvelle_adresse["intitule_voie"] = intitule_voie
+            commune = input("Merci de saisir la commune : ")
+            nouvelle_adresse["commune"] = commune
+            code_postal = input("Merci de saisir le code postal : ")
+            nouvelle_adresse["code_postal"] = code_postal
+            adresses.append(nouvelle_adresse)
         case "2":
-            # edit_adresse = input("Veuillez indiquer votre choix")
-            # print('''
-            #     1 - Modifier le numéro de voie
-            #     2 - Modifier le complément
-            #     3 - Modifier la voie
-            #     4 - Modifier la ville
-            #     5 - Modifier le code postal
-            #     0 - Quitter
-            # ''')
-            # match edit_adresse : 
-            #     case "1": 
-            print("Choix 2")
+            print("===== Edition d'une adresse =====")
+            index_adresse = int(input("Merci de saisir l'index de l'adresse : "))
+            if index_adresse >= 0 and index_adresse < len(adresses):
+                numero_voie = input("Merci de saisir numéro de voie : ")
+                adresses[index_adresse]["numero_voie"] = numero_voie if numero_voie != '' else adresses[index_adresse]["numero_voie"]
+                complement_adresse = input("Merci de saisir le complément d'adresse : ")
+                adresses[index_adresse]["complement_adresse"] = complement_adresse if complement_adresse != '' else adresses[index_adresse]["complement_adresse"]
+                intitule_voie = input("Merci de saisir l'intitulé de la voie : ")
+                adresses[index_adresse]["intitule_voie"] = intitule_voie if intitule_voie != '' else adresses[index_adresse]["intitule_voie"]
+                commune = input("Merci de saisir la commune : ")
+                adresses[index_adresse]["commune"] = commune if commune != '' else adresses[index_adresse]["commune"]
+                code_postal = input("Merci de saisir le code postal : ")
+                adresses[index_adresse]["code_postal"] = code_postal if code_postal != '' else adresses[index_adresse]["code_postal"]
         case "3":
-            print("Choix 3")
+            print("===== Suppression d'une adresse =====")
+            index_adresse = int(input("Merci de saisir l'index de l'adresse : "))
+            if index_adresse >= 0 and index_adresse < len(adresses):
+                adresses.remove(adresses[index_adresse])
         case "4":
-            if len(dico_adresses) > 0:
-                for el in range(0, len(dico_adresses)) :
-                    print(f"{dico_adresses}")
-            else: 
-                print("Aucune adresse")
+            print("===== Affichage des adresses =====")
+            for index, adresse in enumerate(adresses):
+                print(f"-------Adresse n° {index}-------")
+                for cle, valeur in adresse.items():
+                    print(f"{cle} : {valeur}")
         case "0":
             break
+            
